3f53728579d6041608885867e5d0e704dc24ee08
Tom Carmichael-Mhanna <tomcarmichael@hotmail.co.uk>
2023-03-30 11:30:24 +0100

    Project setup for student_directory_2

---
+++ student_directory_2/spec/seeds_students_cohorts.sql
@@ -0,0 +1,18 @@
+-- Create the table without the foreign key first.
+CREATE TABLE cohorts (
+  id SERIAL PRIMARY KEY,
+  name text,
+  start_date date
+);
+
+-- Then the table with the foreign key first.
+CREATE TABLE students (
+  id SERIAL PRIMARY KEY,
+  name text,
+-- The foreign key name is always {other_table_singular}_id
+  cohort_id int,
+  constraint fk_cohort foreign key(cohort_id)
+    references cohorts(id)
+    on delete cascade
+);
+
