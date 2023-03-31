9bf2fb755b6c5638fd6a734e19fb97692a7a35e3
Tom Carmichael-Mhanna <tomcarmichael@hotmail.co.uk>
2023-03-31 12:04:25 +0100

    Add schema recipe and table creation SQL for tagged_students
    ... as part of many-to-many table relationship exercises

---
+++ many_to_many/tagged_students/students_tags.sql
@@ -0,0 +1,20 @@
+-- Create the first table.
+CREATE TABLE students (
+  id SERIAL PRIMARY KEY,
+  name text
+);
+
+-- Create the second table.
+CREATE TABLE tags (
+  id SERIAL PRIMARY KEY,
+  name text
+);
+
+-- Create the join table.
+CREATE TABLE students_tags (
+  student_id int,
+  tag_id int,
+  constraint fk_student foreign key(student_id) references students(id) on delete cascade,
+  constraint fk_tag foreign key(tag_id) references tags(id) on delete cascade,
+  PRIMARY KEY (student_id, tag_id)
+);
